d1b9a8ae0cd42017d2506a4a66843e427a7b3d01
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-05 15:06:01 +0100

    added capacity to docking stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,27 @@
 class DockingStation
 
-  def release_bike
-    Bike.new
+  CAPACITY = 5
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    fail 'No bikes available' if bikes.empty?
+    bikes.pop
+  end
+
+  def dock(bike)
+    fail 'No slots available' if full?
+    bikes << bike
+  end
+
+  def full?
+    bikes.length >= CAPACITY
+  end
+
+  private
+
+  attr_reader :bikes
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,24 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'releases a working bike' do
-    expect(subject.release_bike).to be_working
+
+  describe '#release_bike' do
+    it 'raises an error if no bikes available' do
+      expect{ subject.release_bike }.to raise_error(RuntimeError, 'No bikes available')
+    end
+    it 'releases a working bike if it has one' do
+      subject.dock(Bike.new)
+      expect(subject.release_bike).to be_working
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error if capacity has been reached' do
+      subject.dock(Bike.new) until subject.full?
+      expect{ subject.dock(Bike.new) }.to raise_error(RuntimeError, 'No slots available')
+    end
   end
 
 end
